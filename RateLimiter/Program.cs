077edf37d0b5c80013c8a270cc8067fc600b8a11
Nataliya Pogodina <n.pogodina@gmail.com>
2023-01-12 10:39:36 -0800

    Add project files.

---
+++ RateLimiter/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Azure API Management needs the Swagger definitions to always be present, regardless of the application's environment
+app.UseSwagger();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
